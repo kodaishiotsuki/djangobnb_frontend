e71c692ca6ab0058b2aa4364b55e13769969a1cd
Kodai <0711dr5@gmail.com>
2024-10-07 18:07:33 +0900

    fix:create propertyDetailPage

---
+++ app/properties/[id]/page.tsx
@@ -0,0 +1,50 @@
+import ReservationSidebar from "@/app/components/properties/ReservationSidebar";
+import Image from "next/image";
+
+const PropertyDetailPage = async () => {
+  return (
+    <main className="max-w-[1500px] mx-auto px-6 pb-6">
+      <div className="w-full h-[64vh] mb-4 overflow-hidden rounded-xl relative">
+        <Image
+          fill
+          src="/beach_1.jpg"
+          className="object-cover w-full h-full"
+          alt="Beach house"
+        />
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-5 gap-4">
+        <div className="py-6 pr-6 col-span-3">
+          <h1 className="mb-4 text-4xl">property title</h1>
+
+          <span className="mb-6 block text-lg text-gray-600">
+            2 guests - 4 bedrooms - 1 bathrooms
+          </span>
+
+          <hr />
+
+          <div className="py-6 flex items-center space-x-4">
+            <Image
+              src="/profile_pic_1.jpg"
+              width={50}
+              height={50}
+              className="rounded-full"
+              alt="The user name"
+            />
+            <p>
+              <strong>hoge</strong> is your host
+            </p>
+          </div>
+
+          <hr />
+
+          <p className="mt-6 text-lg">property description</p>
+        </div>
+
+        <ReservationSidebar />
+      </div>
+    </main>
+  );
+};
+
+export default PropertyDetailPage;
